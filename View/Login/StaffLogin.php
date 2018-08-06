1bb68a5b407f51ec47cea0ddb8244cd6936d3820
user <user@HP15-ba010AX>
2018-08-07 00:58:46 +0800

    #153147769

---
+++ View/Login/StaffLogin.php
@@ -0,0 +1,32 @@
+<?php
+ /*<p class="message">Not registered? <a href="#">Create an account</a></p>*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="UTF-8">
+  <link rel="stylesheet"  type="text/css" href="../../Css/LoginSystem.css">
+</head>
+<body>
+<div class="login-page">
+  <div class="form">
+      <h3>Staff Login </h3>
+    <form class="register-form">
+       
+      <input type="text" placeholder="userName"/>
+      <input type="password" placeholder="userPassword"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+     
+    </form>
+  </div>
+</div>
+
+    </body>
+</html>
